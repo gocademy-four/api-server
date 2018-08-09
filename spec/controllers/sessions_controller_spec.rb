176587b0d2aeeeb7447500c707c7562d534798c7
Daniel <nieltansah@gmail.com>
2018-08-09 21:36:15 +0700

    sessions: spec: Update spec.: Handle username, password param.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "POST #login" do
-    it "rejects request with missing parameter" do
+    it "rejects request with missing email" do
       expect {
-        post :login
+        post :login, params: { password: FactoryBot.build(:member).password }
+      }.to raise_error(ActionController::ParameterMissing)
+    end
+
+    it "rejects request with missing password" do
+      expect {
+        post :login, params: { email: FactoryBot.build(:member).email }
       }.to raise_error(ActionController::ParameterMissing)
     end
   end
