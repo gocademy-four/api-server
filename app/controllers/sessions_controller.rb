4d7c7bf383666fa76d7717dfd08bf98064eefa97
Daniel <nieltansah@gmail.com>
2018-08-09 21:37:43 +0700

    sessions: Add routes, login endpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,5 @@
 class SessionsController < ApplicationController
+  def login
+    params.require([:email, :password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello', to: 'hello#test'
+
+  resource :session, only: [:create, :delete] do
+    post 'login', to: 'sessions#login'
+  end
 end
