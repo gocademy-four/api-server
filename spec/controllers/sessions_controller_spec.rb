696fb0ac723717f142a995067159f08263261d32
Daniel <nieltansah@gmail.com>
2018-08-09 19:47:27 +0700

    sessions: spec: Handle rejection of request with missing param.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-
+  describe "POST #login" do
+    it "rejects request with missing parameter" do
+      expect {
+        post :login
+      }.to raise_error(ActionController::ParameterMissing)
+    end
+  end
 end
