480ebccfb259040035906e74a78f4ee8efd56e00
Daniel <nieltansah@gmail.com>
2018-08-12 14:20:53 +0700

    sessions: Add more member associated spec.
    Put single member instance on the top for testing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,16 +2,42 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "POST #login" do
+    let (:member) { FactoryBot.create(:member) }
+
     it "rejects request with missing email" do
       expect {
-        post :login, params: { password: FactoryBot.build(:member).password }
+        post :login, params: { password: member.password }
       }.to raise_error(ActionController::ParameterMissing)
     end
 
     it "rejects request with missing password" do
       expect {
-        post :login, params: { email: FactoryBot.build(:member).email }
+        post :login, params: { email: member.email }
       }.to raise_error(ActionController::ParameterMissing)
+    end
+
+    it "rejects request with unknown member" do
+      post :login, params: {
+        email: "garbage" + member.email,
+        password: member.password
+      }
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "rejects request with known member but wrong password" do
+      post :login, params: {
+        email: member.email,
+        password: "garbage" + member.password
+      }
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "accepts request with known member" do
+      post :login, params: {
+        email: member.email,
+        password: member.password
+      }
+      expect(response).to have_http_status(:ok)
     end
   end
 end
