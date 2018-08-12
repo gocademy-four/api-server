4046c86d510daa9e36f90a28d1b5cd7943140332
Daniel <nieltansah@gmail.com>
2018-08-12 14:20:17 +0700

    models: member: factory: Impl. simple factory

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :member do
-    email "email@email.com"
-    password "password"
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password }
+
+    name { Faker::Name.name }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    gender { Faker::Gender.binary_type }
   end
 end
