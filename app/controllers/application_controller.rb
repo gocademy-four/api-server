d1688f6e48f6603051c56ada29e5f2ceed0fc5d0
Daniel <nieltansah@gmail.com>
2018-08-14 11:15:36 +0700

    sessions: Add a method to get current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  def current_user
+    Member.find_by!(token)
+  end
+
+  private
+  def token
+    http_auth = request.env.fetch("HTTP_AUTHORIZATION", "")
+    token_val = http_auth.scan(/^Bearer (.*)$/).flatten.last
+
+    throw ActionController::BadRequest unless token_val
+    token_val
+  end
 end
