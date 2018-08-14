bf50d281cbddc6654b670dc575b0fbad3d8e1c36
Daniel <nieltansah@gmail.com>
2018-08-14 11:12:41 +0700

    Revert "hello: Add sample controller"
    This reverts commit 96073bfb74e0260f661cf08a081327143df38197.

--- app/controllers/hello_controller.rb
+++
@@ -1,5 +0,0 @@
-class HelloController < ApplicationController
-  def test
-    render json: { this: "is an example of", ruby: "on rails" }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello', to: 'hello#test'
 
   resource :session, only: [:create, :delete] do
     post 'login', to: 'sessions#login'
