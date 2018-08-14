765df8a48e57328d7e9938a17b3c86dc1fc1ade0
Daniel <nieltansah@gmail.com>
2018-08-14 16:39:34 +0700

    customers: Add ability to register customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,3 +1,5 @@
+require 'token_service'
+
 class CustomersController < ApplicationController
   def show
     if params[:id] then
@@ -9,5 +11,17 @@ class CustomersController < ApplicationController
     render json: customer.as_json.except("member_id").merge({
       member: customer.member.as_json.except("password_digest")
     })
+  end
+
+  def create
+    customer = Customer.new
+    customer.member = Member.new(params.require(:member).permit(
+      :email, :name, :phone_number, :gender, :street, :city, :password
+    ))
+    customer.save!
+
+    render json: {
+      token: TokenService.issue(customer.member.id)
+    }
   end
 end
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 require 'token_service'
 
 RSpec.describe CustomersController, type: :controller do
-  let(:customer) { FactoryBot.create(:customer) }
-
   describe "GET #customer" do
+    let(:customer) { FactoryBot.create(:customer) }
+
     context "logged in" do
       before(:each) do
         request.headers['Authorization'] =
           "Bearer " + TokenService.issue(customer.member.id)
-        post :show
+        get :show
       end
 
       it "returns HTTP 200 OK" do
@@ -38,6 +38,32 @@ RSpec.describe CustomersController, type: :controller do
           it "doesn't have password digest" do
             expect(json_member).not_to have_key(:password_digest)
           end
+        end
+      end
+    end
+  end
+
+  describe "POST #customer" do
+    it "rejects request with missing member" do
+      expect {
+        post :create
+      }.to raise_error(ActionController::ParameterMissing)
+    end
+
+    context "valid parameter" do
+      before(:each) do
+        post :create, params: { member: FactoryBot.build(:customer).as_json }
+      end
+
+      it "returns HTTP 200 OK" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      describe "JSON response body" do
+        let(:json) { JSON.parse(response.body, symbolize_names: true) }
+
+        it "has token" do
+          expect(json).to have_key(:token)
         end
       end
     end
