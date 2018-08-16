cbaccebeabbc386735302dc5536e6a04b2564c4a
fairuz <fairuzyassar7@gmail.com>
2018-08-16 21:16:38 +0700

    build order models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_135342) do
+ActiveRecord::Schema.define(version: 2018_08_16_141402) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2018_08_16_135342) do
     t.text "street"
     t.string "city"
     t.string "password_digest"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "teachedlesson_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+    t.index ["teachedlesson_id"], name: "index_orders_on_teachedlesson_id"
   end
 
   create_table "teachedlessons", force: :cascade do |t|
