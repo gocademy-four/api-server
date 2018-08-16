7ef62a5dfc61886f11c267428a3cf5ecf3018076
fairuz <fairuzyassar7@gmail.com>
2018-08-16 22:28:21 +0700

    create order post

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,12 @@
 class OrdersController < ApplicationController
+	def create
+		params.require(:teached_lesson_id)
+
+		order = Order.new
+		order.customer = Customer.find_by!(member_id: current_user.id)
+		order.teachedlesson = Teachedlesson.find_by!(id: params[:teached_lesson_id])
+		order.save!
+
+		render json: {}
+	end
 end
