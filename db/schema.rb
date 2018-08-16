44f1c3b3367b0e0aae9cf88c5023384822daad34
fairuz <fairuzyassar7@gmail.com>
2018-08-16 20:34:36 +0700

    build category and orders controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_041152) do
+ActiveRecord::Schema.define(version: 2018_08_16_133116) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.integer "member_id"
