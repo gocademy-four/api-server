9d07d6abe4c4b15c5cfce631d333417ccec781e0
nicolauscg <nicolausgozali@gmail.com>
2018-08-18 20:50:59 +0700

    add associations between model

--- app/models/teachedlesson.rb
+++ app/models/teachedlesson.rb
@@ -1,4 +1,5 @@
 class Teachedlesson < ApplicationRecord
+  has_many :order
   belongs_to :tutor
   belongs_to :lesson
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,4 +1,5 @@
 class Tutor < ApplicationRecord
   has_many :tutor_regions
+  has_many :teachedlessons
   belongs_to :member
 end
