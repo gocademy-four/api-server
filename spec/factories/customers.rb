0fc919aafd7a024d10deae2e3aa83fcdd97ccd4f
nicolauscg <nicolausgozali@gmail.com>
2018-08-18 16:10:56 +0700

    add factory for customer with many orders

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :customer do
     member
+
+    factory :customer_with_orders do
+      transient do
+        order_count { 3 }
+      end
+
+      after(:create) do |customer, evaluator|
+        create_list(:order, evaluator.order_count, customer: customer)
+      end
+    end
   end
 end
