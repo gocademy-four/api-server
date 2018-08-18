33254cc9eee36eb8b2e6092b33d7597ed04a9881
nicolauscg <nicolausgozali@gmail.com>
2018-08-18 14:28:39 +0700

    fix query with category_id

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -4,7 +4,7 @@ class ClassesController < ApplicationController
         data = Teachedlesson.all
         
         if params[:category_id]
-            lesson_ids = Lesson.where(id: params[:category_id]).ids
+            lesson_ids = Lesson.where(category_id: params[:category_id]).ids
             data = data.where(lesson_id: lesson_ids)
         end
 
